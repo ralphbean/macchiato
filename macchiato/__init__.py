d49fc1953952ad0b3c32cc6eb2cc353ce0ee84fc
Ralph Bean <rbean@redhat.com>
2012-02-02 15:13:24 -0500

    Decorator.

---
+++ macchiato/__init__.py
@@ -0,0 +1,43 @@
+from .compiler import Compiler
+
+from .compiler import Compiler
+
+class steam(object):
+    """ Transmogrifies python functions into javascript """
+
+    def __init__(self, main=None, closure=True):
+        self.main = main
+        self.closure = closure
+
+
+    def __call__(self, func):
+        import ast
+        import inspect
+        import StringIO
+
+        # Build our return buffer
+        buf = StringIO.StringIO()
+
+        # Get the source from the inspect module
+        source = inspect.getsource(func)
+
+        # Strip off the decorator
+        # TODO -- there's probably a better AST way to strip the decorator
+        source = '\n'.join(source.split('\n')[1:])
+
+        tree = compile(source, '', 'exec', ast.PyCF_ONLY_AST)
+        Compiler(tree, buf)
+        result = buf.getvalue()
+
+        # Post processing
+        if self.main:
+            result += '\n' + self.main
+            if result[-1] != ';':
+                result += ';'
+
+        if self.closure:
+            result = "(%s)();" % result
+
+        return result
+
+
