126895ed60ea843af79b766e794d298cf1e1f58a
Ralph Bean <rbean@redhat.com>
2012-03-28 08:31:24 -0400

    Re-added the README string as a runnable test.

---
+++ test.py
@@ -0,0 +1,14 @@
+from macchiato import steamified
+
+@steamified(main="foo()")
+def foo():
+    def bar():
+        x = 0
+        b = 2
+        y = x + b
+        return y
+
+    print bar()
+    return bar()
+
+print foo
